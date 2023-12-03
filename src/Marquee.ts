f1335a9d8ae747e5c92cdf0d0aa0131e648c9da6
anhpn <ngocanhphamk2@gmail.com>
2023-12-03 23:42:54 +0700

    first commit

---
+++ src/Marquee.ts
@@ -0,0 +1,125 @@
+customElements.define(
+  "x-marquee",
+  class extends HTMLElement {
+    #shadowRoot;
+    constructor() {
+      super();
+      let style = this.getAttribute("style") || "";
+      let pauseOnHover = this.getAttribute("pauseOnHover") || false;
+      let pauseOnClick = this.getAttribute("pauseOnClick") || false;
+      let direction = this.getAttribute("direction") || "left";
+      let speed = this.getAttribute("speed") ? Number(this.getAttribute("speed")) : 100;
+      let play = this.getAttribute("play") || true;
+      let gap = this.getAttribute("gap") || "0px";
+      let className = this.getAttribute('class') ?? '';
+
+      this.#shadowRoot = this.attachShadow({ mode: "open" });
+
+      this.#shadowRoot.innerHTML = `
+      <style>
+        .marquee-container {
+          display: flex;
+          width: 100%;
+          overflow-x: hidden;
+          flex-direction: row;
+          position: relative;
+        }
+      
+        .marquee-container:hover .marquee {
+          animation-play-state: var(--pause-on-hover);
+        }
+      
+        .marquee-container:active .marquee {
+          animation-play-state: var(--pause-on-click);
+        }
+      
+        .marquee {
+          flex: 0 0 auto;
+          min-width: 100%;
+          z-index: 1;
+          display: flex;
+          flex-direction: row;
+          align-items: center;
+          animation: scroll var(--duration) linear infinite;
+          animation-play-state: var(--play);
+          animation-direction: var(--direction);
+        }
+      
+        @keyframes scroll {
+          0% {
+            transform: translateX(0%);
+          }
+          100% {
+            transform: translateX(-100%);
+          }
+        }
+      
+        .initial-child-container {
+          flex: 0 0 auto;
+          display: flex;
+          min-width: auto;
+          flex-direction: row;
+        }
+      
+        .gradient::after,
+        .gradient::before {
+          background: linear-gradient(
+            to right,
+            var(--gradientColor, white),
+            transparent
+          );
+          content: "";
+          height: 100%;
+          position: absolute;
+          width: var(--gradientWidth, 10%);
+          z-index: 2;
+        }
+      
+        .gradient::before {
+          left: 0;
+          top: 0;
+        }
+      
+        .gradient::after {
+          right: 0;
+          top: 0;
+          transform: rotateZ(180deg);
+        }
+      </style>
+
+      <div
+        style="${style}
+        --gap: ${gap}; 
+        --play: ${play}; 
+        --direction: ${ direction === "left" ? "normal" : "reverse"}; 
+        --pause-on-hover: ${pauseOnHover ? "paused" : "running"}; 
+        --pause-on-click: ${pauseOnClick ? 'paused' : 'running'}"
+        class="marquee-container ${className}"
+      >
+        <div class="marquee">
+          <slot></slot>
+        </div>
+        <div class="marquee">
+          <slot></slot>
+        </div>
+      </div>
+      `;
+      const marqueeContainer = this.#shadowRoot.querySelector(
+        ".marquee-container"
+      ) as Element;
+      const marquee = this.#shadowRoot.querySelector(
+        ".marquee"
+      ) as Element;
+      const resizeObserver = new ResizeObserver(() => {
+        const duration =
+        marquee.clientWidth < marqueeContainer.clientWidth
+        ? marqueeContainer.clientWidth / speed
+        : marquee.clientWidth / speed
+        this.#shadowRoot.querySelector<HTMLElement>('.marquee-container')?.style.setProperty('--duration', `${duration}s`)
+      });
+
+      resizeObserver.observe(marqueeContainer);
+      resizeObserver.observe(marquee);
+    }
+  }
+);
